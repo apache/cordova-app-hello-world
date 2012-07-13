fc2c23c256d7793d1e27e9d1820c0b7ea6730aed
Michael Brooks <michael@michaelbrooks.ca>
2012-07-12 21:30:14 -0700

    Add app.initialize stub.

---
+++ www/spec/index.js
@@ -0,0 +1,11 @@
+describe('app', function() {
+    it('should exist', function() {
+        expect(app).toBeDefined();
+    });
+
+    describe('initialize', function() {
+        it('should exist', function() {
+            expect(app.initialize).toBeDefined();
+        });
+    });
+});
