22fa90553e1174c64e8928d89ea6f126085709e2
Michael Brooks <michael@michaelbrooks.ca>
2012-07-12 23:04:09 -0700

    Add spec helper.

---
+++ www/spec/helper.js
@@ -0,0 +1,11 @@
+beforeEach(function() {
+    document.getElementById('stage').innerHTML = '';
+});
+
+var helper = {
+    trigger: function(obj, name) {
+        var e = document.createEvent('Event');
+        e.initEvent(name, true, true);
+        obj.dispatchEvent(e);
+    }
+};
