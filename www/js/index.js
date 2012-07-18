f8ae936fede5424d2077c54b681e9f4471e6b829
Jesse MacFadyen <purplecabbage@gmail.com>
2012-07-18 16:10:41 -0700

    removed bind + report methods, removed dep on classList

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,10 @@
 var app = {
     initialize: function() {
-        this.bind();
-    },
-    bind: function() {
         document.addEventListener('deviceready', this.deviceready, false);
     },
     deviceready: function() {
-        app.report('deviceready');
-    },
-    report: function(id) {
-        document.querySelector('#' + id + ' .pending').classList.add('hide');
-        document.querySelector('#' + id + ' .complete').classList.remove('hide');
+        document.querySelector('#deviceready .pending').className += ' hide';
+        var completeElem = document.querySelector('#deviceready .complete');
+        completeElem.className = completeElem.className.split('hide').join('');
     }
 };
