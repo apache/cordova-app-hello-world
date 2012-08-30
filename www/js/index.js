1d98ded89c6b0d944a2b0bab7be03b745659cece
Michael Brooks <michael@michaelbrooks.ca>
2012-08-30 11:04:01 -0700

    [app] Add comments to JavaScript.

--- www/js/index.js
+++ www/js/index.js
@@ -17,17 +17,25 @@
  * under the License.
  */
 var app = {
+    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // `load`, `deviceready`, `offline`, and `online`.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+    // deviceready Event Handler
+    //
+    // The scope of `this` is the event. In order to call the `receivedEvent`
+    // function, we must explicity call `app.receivedEvent(...);`
     onDeviceReady: function() {
-        // This is an event handler function, which means the scope is the event.
-        // So, we must explicitly called `app.report()` instead of `this.report()`.
         app.receivedEvent('deviceready');
     },
+    // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
