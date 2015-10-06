7608c6a389969229a5750360082d18aae82191e6
daserge <v-seshak@microsoft.com>
2015-10-06 13:33:52 +0300

    CB-9712 CLI 5.3 breaks with node 3.3.3
    Added index.js so that module can be required and resolved
    Exposed dirname in module exports

---
+++ index.js
@@ -0,0 +1,5 @@
+// Entry point is needed so that the module could be required.
+// The module dir path will be need to copy resources from it.
+exports = module.exports = {
+    dirname: __dirname
+};
