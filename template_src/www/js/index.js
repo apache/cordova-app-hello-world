20eca27418e69392e6f6f047e015bd777fb90ee9
Raphael von der Grün <raphinesse@gmail.com>
2019-11-21 18:11:38 +0100

    Actually use some Cordova APIs in onDeviceReady

--- template_src/www/js/index.js
+++ template_src/www/js/index.js
@@ -24,6 +24,6 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
-    console.log('Received deviceready event');
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
