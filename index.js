1e22d2fb4ff76953c31d14676e10b80ce08aa75d
エリス <erisu@users.noreply.github.com>
2020-03-31 00:21:38 +0900

    refactor: transform var to let/const (#60)

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
 // Entry point is needed so that the module could be required.
 // The module dir path will be needed to copy resources from it.
 
-var path = require('path');
+const path = require('path');
 
 module.exports = {
     dirname: path.join(__dirname, 'template_src')
